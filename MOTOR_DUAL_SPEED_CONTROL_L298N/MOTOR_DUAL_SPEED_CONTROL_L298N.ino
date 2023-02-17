0edcc67ed15def73cbc71b7088fd120e773feefa
shravista <shravistakashyap@gmail.com>
2023-02-18 00:22:38 +0530

    intiated the arduino codes

---
+++ MOTOR_DUAL_SPEED_CONTROL_L298N/MOTOR_DUAL_SPEED_CONTROL_L298N.ino
@@ -0,0 +1,67 @@
+#define MOTORA_EN 5
+#define MOTORA_IN1 4
+#define MOTORA_IN2 6
+
+#define MOTORB_EN 10
+#define MOTORB_IN1 3
+#define MOTORB_IN2 2
+
+String inputString = "";         // a String to hold incoming data
+bool stringComplete = false;  // whether the string is complete
+
+void setup() {
+  // initialize serial:
+  Serial.begin(9600);
+  // reserve 200 bytes for the inputString:
+  inputString.reserve(200);
+  pinMode(MOTORA_EN, OUTPUT);
+  pinMode(MOTORA_IN1, OUTPUT);
+  pinMode(MOTORA_IN2, OUTPUT);
+
+  pinMode(MOTORB_EN, OUTPUT);
+  pinMode(MOTORB_IN1, OUTPUT);
+  pinMode(MOTORB_IN2, OUTPUT);
+}
+
+int speed = 0;
+
+void loop() {
+  // print the string when a newline arrives:
+  if (stringComplete) {
+    //Serial.println(inputString);
+    // clear the string:
+    stringComplete = false;
+    speed = inputString.toInt();
+    Serial.println(speed);
+    inputString = "";
+  }
+  speed = speed > 255 ? 255 : speed;
+  speed = speed < 0 ? 0: speed;
+  digitalWrite(MOTORA_IN1, HIGH);
+  digitalWrite(MOTORA_IN2, LOW);
+  analogWrite(MOTORA_EN, speed);
+
+  digitalWrite(MOTORB_IN1, HIGH);
+  digitalWrite(MOTORB_IN2, LOW);
+  analogWrite(MOTORB_EN, speed);
+}
+
+/*
+  SerialEvent occurs whenever a new data comes in the hardware serial RX. This
+  routine is run between each time loop() runs, so using delay inside loop can
+  delay response. Multiple bytes of data may be available.
+*/
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    // if the incoming character is a newline, set a flag so the main loop can
+    // do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+      return;
+    }
+    inputString += inChar;
+  }
+}
---
+++ MOTOR_SPEED_CONTROL_L298N/MOTOR_SPEED_CONTROL_L298N.ino
@@ -0,0 +1,55 @@
+#define MOTOR_EN 5
+#define MOTOR_IN1 4
+#define MOTOR_IN2 3
+
+String inputString = "";         // a String to hold incoming data
+bool stringComplete = false;  // whether the string is complete
+
+void setup() {
+  // initialize serial:
+  Serial.begin(9600);
+  // reserve 200 bytes for the inputString:
+  inputString.reserve(200);
+  pinMode(MOTOR_EN, OUTPUT);
+  pinMode(MOTOR_IN1, OUTPUT);
+  pinMode(MOTOR_IN2, OUTPUT);
+}
+
+int speed = 0;
+
+void loop() {
+  // print the string when a newline arrives:
+  if (stringComplete) {
+    //Serial.println(inputString);
+    // clear the string:
+    stringComplete = false;
+    speed = inputString.toInt();
+    Serial.println(speed);
+    inputString = "";
+  }
+  speed = speed > 255 ? 255 : speed;
+  speed = speed < 0 ? 0: speed;
+  digitalWrite(MOTOR_IN1, HIGH);
+  digitalWrite(MOTOR_IN2, LOW);
+  analogWrite(MOTOR_EN, speed);
+}
+
+/*
+  SerialEvent occurs whenever a new data comes in the hardware serial RX. This
+  routine is run between each time loop() runs, so using delay inside loop can
+  delay response. Multiple bytes of data may be available.
+*/
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    // if the incoming character is a newline, set a flag so the main loop can
+    // do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+      return;
+    }
+    inputString += inChar;
+  }
+}
